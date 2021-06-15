7988d1304501ade634ca0f8b2fc690fcca3f4539
BuildTools <semgo@gmail.com>
2021-06-16 00:38:54 +0300

    add global store

---
+++ lib/redux/actions/SignUpActions.dart
@@ -0,0 +1,5 @@
+class SignUpSetCookie {
+  final String cookie;
+
+  SignUpSetCookie(this.cookie);
+}
--- lib/redux/redux.dart
+++ lib/redux/redux.dart
@@ -1,1 +1,29 @@
-class State {}
+//описываем состояние
+import 'package:diary_app/redux/actions/SignUpActions.dart';
+
+class State {
+  SignUp signUp = SignUp();
+}
+
+class SignUp {
+  String? cookie;
+}
+
+//обрабатываем события
+State appReducers(State state, dynamic action) {
+  if (action is SignUpSetCookie) {
+    //что-то делаем... вызаваем функцию для обработки и т.д
+    return setCookie(state, action);
+  }
+  return state;
+}
+
+State setCookie(State state, SignUpSetCookie action) {
+  state.signUp.cookie = action.cookie;
+  return state;
+}
+
+// List<CartItem> toggleItemState(List<CartItem> items, ToggleItemStateAction action) {
+//   return items.map((item) => item.name == action.item.name ?
+//     action.item : item).toList();
+// }
