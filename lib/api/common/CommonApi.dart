aebcda46dcf50cba5044129890a9be4e16feb7e3
semgoSE <semgoSE@gmail.com>
2021-07-01 00:42:44 +0300

    add common api for server

---
+++ lib/api/common/CommonApi.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:diary_app/api/config.dart';
+import 'package:http/http.dart' as http;
+
+class CommonApi {
+
+  String? url_server; //адресс сервера
+  Uri? route;//марщрут
+
+  String? json_body; //тело запроса
+
+  CommonApi() {
+    this.url_server = URL;
+    this.route = new Uri(path: url_server);
+  }
+
+
+ //редактируем маршрут
+  void setPath(String path) {
+    route = new Uri(path: url_server! + path);
+  }
+
+  //редактируем ответ
+  void setBody(Object obj) {
+    json_body = jsonEncode(obj);
+  }
+
+
+
+  //отправляем запрос
+  Future<dynamic> request() async {
+    http.post(route!, body: json_body).then((response) {
+      print("Response status: ${response.statusCode}");
+      print("Response body: ${response.body}");
+      return jsonDecode(response.body);
+    }).catchError((error){
+      print("Не работает инет");
+    });
+  }
+
+}
---
+++ lib/api/config.dart
@@ -0,0 +1,1 @@
+const String URL = ""; //адрес сервера
