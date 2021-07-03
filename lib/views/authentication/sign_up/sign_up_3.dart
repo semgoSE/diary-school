2b8568f43b25baa1fc75f199891f6df445fb1333
semgose <semgose@gmail.com>
2021-07-03 16:39:14 +0300

    добавил авторизацию через VK. не тестировал

--- lib/views/authentication/sign_up/sign_up_3.dart
+++ lib/views/authentication/sign_up/sign_up_3.dart
@@ -1,16 +1,12 @@
 import 'package:diary_app/components/button.dart';
-import 'package:diary_app/components/form_item.dart';
-import 'package:diary_app/components/input.dart';
-import 'package:diary_app/components/select_mimicry.dart';
 import 'package:diary_app/components/simple_cell.dart';
 import 'package:diary_app/models/AccountBind.dart';
-import 'package:diary_app/redux/actions/AddLoginAndPasswordAndRegionSignUp.dart';
 import 'package:diary_app/redux/redux.dart';
-import 'package:diary_app/views/authentication/sign_up/server_url_argement.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:flutter_login_vk/flutter_login_vk.dart';
 
 class SignUp3 extends StatefulWidget {
   @override
@@ -19,13 +15,48 @@ class SignUp3 extends StatefulWidget {
 
 class SignUp3State extends State<SignUp3> {
 
-  List<AccountBind> accounts_bind = [];
+  List<AccountBindView> accounts_bind = [];
+
+  void bindVK() async {
+    var vk = VKLogin();
+    await vk.initSdk("7847370");
+    var res = await vk.logIn();
+    if (res.isValue) {
+      final VKLoginResult data = res.asValue!.value;
+      if (!data.isCanceled) {
+        final VKAccessToken accessToken = data.accessToken!;
+        final d = await vk.getUserProfile();
+        final VKUserProfile? user = d.asValue!.value;
+        setState(() {
+          accounts_bind.add(AccountBindView(name: user!.firstName + " " + user.lastName, photo: user.photo200, accountBind: AccountBind(uuid: user.userId, token: accessToken.token)));
+        });
+        vk.logOut();
+        Navigator.pop(context);
+      }
+    }
+  }
+
+  void showModal() {
+    showBarModalBottomSheet(context: context, builder: (context) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AppBar(
+              title: Text("Выберите"),
+              automaticallyImplyLeading: false,
+              textTheme: Theme.of(context).textTheme,
+              centerTitle: true),
+          SimpleCell(child: Text("Вконтакте"), onClick: bindVK)
+        ],
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<StateStore,dynamic>(
-      converter: (store) => store,
-      builder: (BuildContext context, store) {
+    return StoreConnector<StateStore,SignUp>(
+      converter: (store) => store.state.signUp,
+      builder: (BuildContext context, signUp) {
         return Scaffold(
             backgroundColor: Theme.of(context).backgroundColor,
             appBar: AppBar(
@@ -44,7 +75,10 @@ class SignUp3State extends State<SignUp3> {
                     child: Text("Добавить привязку"),
                     before: Icon(Icons.add),
                   ),
-                  Expanded(child: ListView.builder(itemBuilder: (context, i) => SimpleCell(child: ))),
+                  Expanded(child: ListView(children: 
+                    accounts_bind.map((e) => SimpleCell(child: Text(e.name!))).toList(),
+                    physics: PageScrollPhysics(),
+                  )),
                    Container(child: MyButton(
                     mode: "commerce",
                     child: "Готов",
@@ -56,4 +90,12 @@ class SignUp3State extends State<SignUp3> {
       },
     );
   }
+}
+
+
+class AccountBindView {
+  String? name;
+  String? photo;
+  AccountBind? accountBind;
+  AccountBindView({ this.name, this.photo, this.accountBind});
 }
