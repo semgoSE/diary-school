56e5f5d24799c4ba252d3039e3f5a075df6d3591
ZloyPokemon <diman2k20@yandex.ru>
2021-09-01 20:31:53 +0300

    Подробный урок основа

--- lib/views/root/SheduleWeek/SheduleModalLessonInfo.dart
+++ lib/views/root/SheduleWeek/SheduleModalLessonInfo.dart
@@ -1,3 +1,4 @@
+import 'package:diary_app/components/spinner.dart';
 import 'package:diary_app/mobX/shedule_week.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,9 @@ class SheduleModalLessonInfo extends StatefulWidget {
 }
 
 class SheduleModalLessonInfoState extends State {
+  bool assessmentLoading = true; //загрузка оценок
+  bool seeTeacher = false; //учитель / заглушка
+
   @override
   void initState() {
     super.initState();
@@ -18,6 +22,8 @@ class SheduleModalLessonInfoState extends State {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Observer(builder: (_) {
       SheduleWeek sheduleWeek = Provider.of<SheduleWeek>(context);
       return Container(
@@ -29,9 +35,109 @@ class SheduleModalLessonInfoState extends State {
               textTheme: Theme.of(context).textTheme,
               automaticallyImplyLeading: false,
             ),
+            getGroup(
+              width,
+              height,
+              "Оценки",
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: height * 0.03,
+                ),
+                child: assessmentLoading
+                    ? Spinner(
+                        size: height * 0.03,
+                      )
+                    : Center(
+                        child: Text(
+                          "Здесь пока пусто",
+                          style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+              ),
+              true,
+            ),
+            getGroup(
+              width,
+              height,
+              "Учитель",
+              Container(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: height * 0.05,
+                  ),
+                  child: seeTeacher
+                      ? Container()
+                      : Center(
+                          child: Text(
+                            "Здесь пока пусто",
+                            style: TextStyle(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+              true,
+            ),
+            getGroup(
+              width,
+              height,
+              "Подробнее",
+              Container(),
+              false,
+            ),
+            Container(
+              height: 20,
+            ),
           ],
         ),
       );
     });
+  }
+
+  Widget getGroup(
+    double width,
+    double height,
+    String groupName,
+    Widget child,
+    bool seporator,
+  ) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          width: width,
+          margin: EdgeInsets.only(
+            top: height * 0.02,
+            left: width * 0.04,
+            bottom: height * 0.034,
+          ),
+          child: Text(
+            groupName,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        child,
+        seporator
+            ? Container(
+                margin: EdgeInsets.symmetric(horizontal: width * 0.04),
+                height: 2.0,
+                decoration: BoxDecoration(
+                    color: Color(0xFFDEDEDE),
+                    borderRadius: BorderRadius.circular(5.0)),
+              )
+            : Container(),
+      ],
+    );
   }
 }
